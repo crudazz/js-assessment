0e718b8751a139383faa4ff6e7540ab6488fde61
crudazz <romerocruz@gmail.com>
2019-01-17 09:59:16 -0500

    feat: es6 Asynchronously

--- app/async.js
+++ app/async.js
@@ -6,8 +6,8 @@ asyncAnswers = {
    * @param value - Any value
    * @returns {then: function} A promise like object containing a then property.
    */
-  async: function async(value) {
-
+  async: async function async(value) {
+    return (value);
   },
 
   /**
@@ -20,7 +20,14 @@ asyncAnswers = {
    * @param {String} url - a valid url
    * @returns {then: function} A promise like object containing a then property.
    */
-  manipulateRemoteData: function manipulateRemoteData(url) {
-
+  manipulateRemoteData: async function manipulateRemoteData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      let people = data.people;
+      return people.map(x => x.name).sort();
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
